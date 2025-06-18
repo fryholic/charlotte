de2b24c9dae5a05cbabfd040368c2381d6b44f9c
fryholic <fryholic@gmail.com>
2025-06-19 03:49:05 +0900

    Remove deprecated test scripts for deezspot and Spotify API

--- test_spotify_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Docker 컨테이너에서 Spotify API 테스트 실행
-echo "Building the Docker image..."
-docker compose build
-
-echo "Running Spotify API test in Docker container..."
-docker compose run --rm charlotte python test_spotify_api.py
